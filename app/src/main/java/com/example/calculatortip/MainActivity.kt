137a4c0eee63ba21843084927c4c92c83cde6f00
artem1187 <bukinartem11@gmail.com>
2025-10-06 18:14:30 +0700

    Text fields

--- app/src/main/java/com/example/calculatortip/MainActivity.kt
+++ app/src/main/java/com/example/calculatortip/MainActivity.kt
@@ -8,20 +8,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,19 +56,43 @@ fun DemoText(message: String, fontSize: Float) {
 fun DemoScreen(modifier: Modifier = Modifier) {
     var sliderPosition by remember { mutableFloatStateOf(20f) }
     val handlePositionChange = { position : Float ->
-        sliderPosition = position
-    }
+        sliderPosition = position}
+    var summa by remember { mutableStateOf("") }
+    var kolvo by remember { mutableStateOf("") }
+
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
     ) {
+    OutlinedTextField(
+        value = summa,
+        onValueChange = {summa = it},
+        label = { Text("Сумма заказа") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth()
+    )
 
+    OutlinedTextField(
+
+        value = kolvo,
+        onValueChange = { kolvo = it },
+        label = { Text("Количество блюд") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth()
+    )
+
+
+        Spacer(modifier = Modifier.height(150.dp))
         DemoSlider(
             sliderPosition = sliderPosition,
             onPositionChange = handlePositionChange
         )
-
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            text = sliderPosition.toInt().toString() + "sp"
+        )
     }
 }
 
